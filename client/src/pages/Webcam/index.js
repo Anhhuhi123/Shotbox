b14caea120217567239d66d934df362779f9875a
Nguyễn Quốc Anh <162429561+Anhhuhi123@users.noreply.github.com>
2024-10-25 15:13:20 +0700

    dia chi cloud

--- client/src/pages/Webcam/index.js
+++ client/src/pages/Webcam/index.js
@@ -11,7 +11,8 @@ function Webcam() {
     const [capturedImage, setCapturedImage] = useState(null);
     // console.log(capturedImage)
     const handleAddImages = async () => {
-        const CLOUD_NAME = 'djfgf1byn';
+        //const CLOUD_NAME = 'djfgf1byn';
+        const CLOUD_NAME = 'dnouyejs5';
         const PRESET_NAME = 'demo-upload';
         const FOLDER_NAME = 'Demo';
         const api = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
